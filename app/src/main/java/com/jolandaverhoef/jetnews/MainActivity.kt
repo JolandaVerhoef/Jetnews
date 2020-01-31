8d8dc5ba56008031ffb740fbb8c0554b5b982b4c
Jolanda Verhoef <jolanda.verhoef@gmail.com>
2020-01-31 15:52:22 +0100

    Add basic surfaces

--- app/src/main/java/com/jolandaverhoef/jetnews/MainActivity.kt
+++ app/src/main/java/com/jolandaverhoef/jetnews/MainActivity.kt
@@ -3,10 +3,18 @@ package com.jolandaverhoef.jetnews
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+
+val BLUE = Color(0xff7189bf)
+val PINK = Color(0xffdf7599)
+val YELLOW = Color(0xffffc785)
+val GREEN = Color(0xff72d6c9)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +29,10 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(modifier = LayoutWidth.Fill) {
+        Surface(color = BLUE, modifier = LayoutWidth.Fill + LayoutHeight.Min(54.dp)) {}
+        Surface(color = PINK, modifier = LayoutWidth.Fill + LayoutFlexible(1f)) {}
+    }
 }
 
 @Preview
